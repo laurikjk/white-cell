eb65177d8303beaa21eecc4c84b386239f3de524
laurikjk <kajakko.lauri@gmail.com>
2023-07-05 15:51:04 +0300

    Add a little preview ellipse to the ellipse form

--- app/components/EllipseForm.tsx
+++ app/components/EllipseForm.tsx
@@ -3,17 +3,47 @@ import { useState } from "react";
 import FloatInput from "./FloatInput";
 import 'katex/dist/katex.min.css';
 import Latex from 'react-latex-next';
+import EllipseMath from "../math/Ellipse";
+import { getEllipseSVGParams } from "./utils";
 
 const enabledBtnStyle = "bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded";
 const disabledBtnStyle = "bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded cursor-not-allowed";
 
 // ellipse equation: x^2/a^2 + y^2/b^2 = c
+
+const EllipsePreview = ({ a, b, c }: Ellipse) => {
+  const ellipse = new EllipseMath(a, b, c);
+  const { viewBox, rx, ry } = getEllipseSVGParams(ellipse);
+  return (
+  <svg
+    width="100%"
+    height="100%"
+    viewBox={viewBox}
+  >
+    <ellipse  
+      cx={0}
+      cy={0}
+      rx={rx}
+      ry={ry}
+      style={{
+        fillOpacity: 0,
+        strokeWidth: rx / 100,
+        stroke: "white",
+      }}
+    />
+  </svg>
+  );
+}
+
 const EllipseEquation = ({ a, b, c }: { a: number | null; b: number | null; c: number | null }) => {
   return (
     <div className="flex flex-row items-center justify-center p-4">
       <Latex>
         {`$\\frac{x^2}{${a || "a"}^2} + \\frac{y^2}{${b || "b"}^2} = ${c || "c"}$`}
       </Latex>
+      <div style={{ width: "100px", height: "100px" }}>
+        {a !== null && b !== null && c !== null && (<EllipsePreview a={a} b={b} c={c} />)}
+      </div>
     </div>
   );
 }
